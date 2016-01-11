7d029a64f69d7c1b4727acc8773f416728cca846
Alex Larson and Michael Lor <michael.vang.1985@gmail.com>
2016-01-11 13:44:20 -0800

    add definitions to list

--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,11 @@
 $(function(){
 
+
+
   $("#h2java").click(function(){
-    $("#java").toggle();
+    $("#java").toggle("slow");
+    $(".new").toggleCLass("bigger")
+
   });
 
   $("#h2mod").click(function(){
@@ -24,33 +28,55 @@ $(function(){
     $("#plus").toggle();
   });
 
-  $("#h2java").click(function(){
-    $("#java").toggle();
+  $("#h2arg").click(function(){
+    $("#arg").toggle();
   });
 
-  $("#h2java").click(function(){
-    $("#java").toggle();
+  $("#h2string").click(function(){
+    $("#string").toggle();
   });
 
-  $("#h2java").click(function(){
-    $("#java").toggle();
+  $("#h2chain").click(function(){
+    $("#chain").toggle();
   });
 
-  $("#h2java").click(function(){
-    $("#java").toggle();
+  $("#h2esc").click(function(){
+    $("#esc").toggle();
   });
 
-  $("#h2java").click(function(){
-    $("#java").toggle();
+  $("#h2alert").click(function(){
+    $("#alert").toggle();
   });
 
-  $("#h2java").click(function(){
-    $("#java").toggle();
+  $("#h2comm").click(function(){
+    $("#comm").toggle();
   });
 
+  $("#h2bool").click(function(){
+    $("#bool").toggle();
+  });
 
+  $("#h2undef").click(function(){
+    $("#under").toggle();
+  });
 
+  $("#h2param").click(function(){
+    $("#param").toggle();
+  });
 
+  $("#h2funct").click(function(){
+    $("#funct").toggle();
+  });
+
+  $("#h2method").click(function(){
+    $("#method").toggle();
+  });
+
+  // $("h2").click(function () {
+  //   $("p").addClass("blue");
+  //
+  // })
+  //
 
 
 
