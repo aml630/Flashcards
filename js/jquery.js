0667428d8e22ad88eaef36f422ba88b75621dd99
Alex Larson and Michael Lor <alex.m.larson@gmail.com>
2016-01-11 11:42:40 -0800

    edit css to match html

--- js/jquery.js
+++ js/jquery.js
@@ -4,7 +4,49 @@ $(function(){
     $("#java").toggle();
   });
 
+  $("#h2mod").click(function(){
+    $("#mod").toggle();
+  });
 
+  $("#h2oper").click(function(){
+    $("#oper").toggle();
+  });
+
+  $("#h2nan").click(function(){
+    $("#nan").toggle();
+  });
+
+  $("#h2flash").click(function(){
+    $("#flash").toggle();
+  });
+
+  $("#h2plus").click(function(){
+    $("#plus").toggle();
+  });
+
+  $("#h2java").click(function(){
+    $("#java").toggle();
+  });
+
+  $("#h2java").click(function(){
+    $("#java").toggle();
+  });
+
+  $("#h2java").click(function(){
+    $("#java").toggle();
+  });
+
+  $("#h2java").click(function(){
+    $("#java").toggle();
+  });
+
+  $("#h2java").click(function(){
+    $("#java").toggle();
+  });
+
+  $("#h2java").click(function(){
+    $("#java").toggle();
+  });
 
 
 
