81e361388d72db2b0f685a00bd8fc965f801c7de
Alex Larson and Michael Lor <alex.m.larson@gmail.com>
2016-01-11 16:17:50 -0800

    cleaned code with this keyword

--- js/jquery.js
+++ js/jquery.js
@@ -1,142 +1,156 @@
 $(function(){
 
-$("h2").click(function () {
-  $("p").hide();
-  $("div").removeClass("bigger");
+// $("h2").click(function () {
+// $(this).next().toggle()
+// $(this).last().toggleClass("bigger")
+// })
 
+$(".flashcard").click(function () {
+$(this).find("p").toggle("slow")
+$(this).toggleClass("bigger")
 })
-
-  $("#h2java").click(function(){
-    $("#java").toggle("slow");
-    $(".new1").toggleClass("bigger")
-
-  });
-
-  $("#h2mod").click(function(){
-    $("#mod").toggle("slow");
-    $(".new2").toggleClass("bigger")
-
-  });
-
-  $("#h2oper").click(function(){
-    $("#oper").toggle("slow");
-    $(".new3").toggleClass("bigger")
-
-  });
-
-  $("#h2nan").click(function(){
-    $("#nan").toggle("slow");
-    $(".new4").toggleClass("bigger")
-
-  });
-
-  $("#h2flash").click(function(){
-    $("#flash").toggle("slow");
-    $(".new5").toggleClass("bigger")
-
-  });
-
-  $("#h2plus").click(function(){
-    $("#plus").toggle("slow");
-    $(".new6").toggleClass("bigger")
-
-  });
-
-  $("#h2arg").click(function(){
-    $("#arg").toggle("slow");
-    $(".new9").toggleClass("bigger")
-
-  });
-
-  $("#h2string").click(function(){
-    $("#string").toggle("slow");
-    $(".new10").toggleClass("bigger")
-
-  });
-
-  $("#h2chain").click(function(){
-    $("#chain").toggle("slow");
-    $(".new11").toggleClass("bigger")
-
-  });
-
-  $("#h2esc").click(function(){
-    $("#esc").toggle("slow");
-    $(".new12").toggleClass("bigger")
-
-  });
-
-  $("#h2alert").click(function(){
-    $("#alert").toggle("slow");
-    $(".new13").toggleClass("bigger")
-
-  });
-
-  $("#h2comm").click(function(){
-    $("#comm").toggle("slow");
-    $(".new14").toggleClass("bigger")
-
-  });
-
-  $("#h2bool").click(function(){
-    $("#bool").toggle("slow");
-    $(".new15").toggleClass("bigger")
-
-  });
-
-  $("#h2undef").click(function(){
-    $("#undef").toggle("slow");
-    $(".new16").toggleClass("bigger")
-
-  });
-
-  $("#h2param").click(function(){
-    $("#param").toggle("slow");
-    $(".new17").toggleClass("bigger")
-
-  });
-
-  $("#h2funct").click(function(){
-    $("#funct").toggle("slow");
-    $(".new7").toggleClass("bigger")
-
-  });
-
-  $("#h2method").click(function(){
-    $("#method").toggle("slow");
-    $(".new8").toggleClass("bigger")
-
-  });
-
-  $("#h2return").click(function(){
-    $("#return").toggle("slow");
-    $(".new18").toggleClass("bigger")
-
-  });
-
-  // $(".click").click(function() {
-  //   $(".footer").append("<button class='click'>Click Me</button>");
-  // });
-
-  $(".click").click(function() {
-    $(".hiddenimg").slideToggle();
-  });
-
-  // $("h2").click(function () {
-  //   $("p").addClass("blue");
-  //
-  // })
-  //
-
-$(".images").click(function() {
-  event.preventDefault();
-})
-
-
-
-
-
-
-
 
 });
+
+// this keywork makes all below unnecessary
+
+// $("h2").click(function () {
+//   $("p").hide();
+//   $("div").removeClass("bigger");
+//
+// })
+//
+//   $("#h2java").click(function(){
+//     $("#java").toggle("slow");
+//     $(".new1").toggleClass("bigger")
+//
+//   });
+//
+//   $("#h2mod").click(function(){
+//     $("#mod").toggle("slow");
+//     $(".new2").toggleClass("bigger")
+//
+//   });
+//
+//   $("#h2oper").click(function(){
+//     $("#oper").toggle("slow");
+//     $(".new3").toggleClass("bigger")
+//
+//   });
+//
+//   $("#h2nan").click(function(){
+//     $("#nan").toggle("slow");
+//     $(".new4").toggleClass("bigger")
+//
+//   });
+//
+//   $("#h2flash").click(function(){
+//     $("#flash").toggle("slow");
+//     $(".new5").toggleClass("bigger")
+//
+//   });
+//
+//   $("#h2plus").click(function(){
+//     $("#plus").toggle("slow");
+//     $(".new6").toggleClass("bigger")
+//
+//   });
+//
+//   $("#h2arg").click(function(){
+//     $("#arg").toggle("slow");
+//     $(".new9").toggleClass("bigger")
+//
+//   });
+//
+//   $("#h2string").click(function(){
+//     $("#string").toggle("slow");
+//     $(".new10").toggleClass("bigger")
+//
+//   });
+//
+//   $("#h2chain").click(function(){
+//     $("#chain").toggle("slow");
+//     $(".new11").toggleClass("bigger")
+//
+//   });
+//
+//   $("#h2esc").click(function(){
+//     $("#esc").toggle("slow");
+//     $(".new12").toggleClass("bigger")
+//
+//   });
+//
+//   $("#h2alert").click(function(){
+//     $("#alert").toggle("slow");
+//     $(".new13").toggleClass("bigger")
+//
+//   });
+//
+//   $("#h2comm").click(function(){
+//     $("#comm").toggle("slow");
+//     $(".new14").toggleClass("bigger")
+//
+//   });
+//
+//   $("#h2bool").click(function(){
+//     $("#bool").toggle("slow");
+//     $(".new15").toggleClass("bigger")
+//
+//   });
+//
+//   $("#h2undef").click(function(){
+//     $("#undef").toggle("slow");
+//     $(".new16").toggleClass("bigger")
+//
+//   });
+//
+//   $("#h2param").click(function(){
+//     $("#param").toggle("slow");
+//     $(".new17").toggleClass("bigger")
+//
+//   });
+//
+//   $("#h2funct").click(function(){
+//     $("#funct").toggle("slow");
+//     $(".new7").toggleClass("bigger")
+//
+//   });
+//
+//   $("#h2method").click(function(){
+//     $("#method").toggle("slow");
+//     $(".new8").toggleClass("bigger")
+//
+//   });
+//
+//   $("#h2return").click(function(){
+//     $("#return").toggle("slow");
+//     $(".new18").toggleClass("bigger")
+//
+//   });
+//
+//   // $(".click").click(function() {
+//   //   $(".footer").append("<button class='click'>Click Me</button>");
+//   // });
+//
+//   $(".click").click(function() {
+//     $(".hiddenimg").slideToggle();
+//   });
+//
+//   // $("h2").click(function () {
+//   //   $("p").addClass("blue");
+//   //
+//   // })
+//   //
+//
+// $(".images").click(function() {
+//   event.preventDefault();
+// })
+//
+
+
+
+
+
+
+
+// });
